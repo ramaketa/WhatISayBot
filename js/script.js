2c53194666392f80f7bcd5dfe64edc672c180f5b
Ramazan <ramziknur05@gmail.com>
2019-12-09 00:21:50 +0400

    Adaptive

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 function pageUp() {
     window.scrollTo(0,0);
 }
+
+function showMenu() {
+    dropDown = document.getElementById('header-navigation-panel-min');
+
+    if (dropDown.style.display === 'block') {
+        dropDown.style.display = 'none';
+    } else {
+        dropDown.style.display = 'block';
+    }
+}
